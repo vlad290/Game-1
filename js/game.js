22a68116e89afdebfe9bbb874ed8873c69b4c2a9
vlad290 <65389343+vlad290@users.noreply.github.com>
2021-08-19 16:42:23 +0300

    Fix
    Game version v0.1.1

--- js/game.js
+++ js/game.js
@@ -13,13 +13,45 @@ var radiusMass = [10,20,30,40,50,60]; //массив радиуса
 var sizeK = [10,20,30,40,50,60,70,80,90,100]; //массив Ширины
 var sizeT = [[-40, 0, 0, -100, 40, 0],[-40, 0, 0, 100, 40, 0],[100, 0, 0, 40, 0, -40],[-100, 0, 0, 40, 0, -40]]
 var colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba]; //массив цветов
+//Конец Массивов для рандома
 //Звуковые файлы
 var good = new Audio();
 var error = new Audio();
 good.src = "sound/chime.mp3";
 error.src = "sound/boing.mp3";
 //Конец Звуковых файлов
-//Конец Массивов для рандома
+//Отрисовка линии ифигур для перетаскивания
+//Линия раздела
+var graphics = new PIXI.Graphics();
+graphics.lineStyle(5, 0x33FF00);
+graphics.moveTo(0,400);
+graphics.lineTo(width, 400);
+renderer.stage.addChild(graphics);
+//Круг 2
+var circle2 = new PIXI.Graphics();
+circle2.lineStyle(1,0xFFFFFF);
+circle2.beginFill();
+circle2.drawCircle(0, 0, 80);
+circle2.x  = 166;
+circle2.y  = 500;
+renderer.stage.addChild(circle2);
+//Квадрат 2
+const filledRect2 = new PIXI.Graphics()
+filledRect2.lineStyle(1,0xFFFFFF);
+filledRect2.beginFill()
+filledRect2.drawRect(-80, -80, 160, 160)
+filledRect2.x  = 412;
+filledRect2.y  = 500;
+renderer.stage.addChild(filledRect2)
+//Триугольник 2
+const rectangle2 = new PIXI.Graphics();
+rectangle2.lineStyle(1,0xFFFFFF);
+rectangle2.beginFill(); // Color it black
+rectangle2.drawPolygon(-80, 0, 0, -160, 80, 0);
+rectangle2.x  = 658;
+rectangle2.y  = 580;
+renderer.stage.addChild(rectangle2);
+//Конец Отрисовки линии и фигурок
 class Circle
 {
 	constructor(x, y, colors, size)
@@ -144,7 +176,7 @@ class Rectangle
 			this.alpha = 1;
 			this.dragging = false;
 			this.data = null;
-			if ((this.position.x > rectangle2.x - 80 && this.position.x < rectangle2.x + 80) && (this.position.y > rectangle2.y - 80 && this.position.y < rectangle2.y + 80))
+			if ((this.position.x > rectangle2.x - 80 && this.position.x < rectangle2.x + 80) && (this.position.y > rectangle2.y - 180 && this.position.y < rectangle2.y + 180))
 			{
 				this.clear();
 				good.play();
@@ -173,38 +205,6 @@ for (var i = 0; i < 3; i++)
     new FilledRect(Math.floor(Math.random() * 700), Math.floor(Math.random() * 300),colors[randF],sizeK[randomK]);
     new Rectangle(Math.floor(Math.random() * 700), Math.floor(Math.random() * 300),colors[randT],sizeT[randomFTP]);
 }
-//Отрисовка линии ифигур для перетаскивания
-//Линия раздела
-var graphics = new PIXI.Graphics();
-graphics.lineStyle(5, 0x33FF00);
-graphics.moveTo(0,400);
-graphics.lineTo(width, 400);
-renderer.stage.addChild(graphics);
-//Круг 2
-var circle2 = new PIXI.Graphics();
-circle2.lineStyle(1,0xFFFFFF);
-circle2.beginFill();
-circle2.drawCircle(0, 0, 80);
-circle2.x  = 166;
-circle2.y  = 500;
-renderer.stage.addChild(circle2);
-//Квадрат 2
-const filledRect2 = new PIXI.Graphics()
-filledRect2.lineStyle(1,0xFFFFFF);
-filledRect2.beginFill()
-filledRect2.drawRect(-80, -80, 160, 160)
-filledRect2.x  = 412;
-filledRect2.y  = 500;
-renderer.stage.addChild(filledRect2)
-//Триугольник 2
-const rectangle2 = new PIXI.Graphics();
-rectangle2.lineStyle(1,0xFFFFFF);
-rectangle2.beginFill(); // Color it black
-rectangle2.drawPolygon(-80, 0, 0, -160, 80, 0);
-rectangle2.x  = 658;
-rectangle2.y  = 580;
-renderer.stage.addChild(rectangle2);
-//Конец Отрисовки линии и фигурок
 function OnDragStart(event)
 {
     this.data = event.data;
